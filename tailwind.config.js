594a79a3d068810745f20d104a749d099c4249a0
DungIT910 <Dungtb611@gmail.com>
2024-11-29 02:17:52 +0700

    feat: story and subheadline

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,29 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        Karla: ['Karla', 'san-serif ']
+        Karla: ["Karla", "san-serif "],
       },
       colors: {
-        'light-coffee': '#C89F94'
+        coffee: {
+          50: "#E8D6D0",
+          200: "#C89F94",
+          400: "#A25F4B",
+          600: "#744838",
+        },
       },
       keyframes: {
         slideDown: {
-          '0%': { transform: 'translateY(-100%)' },
-          '100%': { transform: 'translateY(0)' },
-        }
+          "0%": { transform: "translateY(-100%)" },
+          "100%": { transform: "translateY(0)" },
+        },
       },
       animation: {
-        slideDown: 'slideDown 0.4s ease-in-out',
+        slideDown: "slideDown 0.4s ease-in-out",
       },
       backgroundImage: {
-        'slider-bg': 'url("./img/slider-bg.jpg")'
-      }
+        "slider-bg": 'url("./img/slider-bg.jpg")',
+      },
     },
   },
   plugins: [],
-}
+};
